0274f50ea6949bba6f73f6c5e6deae9ea432b29f
Sushma <sushmashenoy1997@gmail.com>
2023-05-21 20:58:06 -0700

    file changes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,16 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss'],
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.scss"],
 })
 export class AppComponent {
+  isDesktop = false;
+
   constructor() {
-    let sec = document.addEventListener('scroll', () => {
+    if (window.innerWidth > 500) this.isDesktop = true;
+    let sec = document.addEventListener("scroll", () => {
       this.hovers();
       var st = window.pageYOffset || document.documentElement.scrollTop; // Credits: "https://github.com/qeremy/so/blob/master/so.dom.js#L426"
       if (
@@ -15,9 +18,9 @@ export class AppComponent {
         (document.body.scrollTop > 550 ||
           document.documentElement.scrollTop > 550)
       ) {
-        document.getElementById('header')?.classList.add('growHader');
+        document.getElementById("header")?.classList.add("growHader");
       } else if (st < lastScrollTop && window.scrollY < 550) {
-        document.getElementById('header')?.classList.remove('growHader');
+        document.getElementById("header")?.classList.remove("growHader");
 
         // upscroll code
       } // else was horizontal scroll
@@ -28,23 +31,23 @@ export class AppComponent {
     console.log(sec);
   }
   hovers() {
-    let sections = document.querySelectorAll('section');
-    let headerOptions = document.querySelectorAll('ul li a');
+    let sections = document.querySelectorAll("section");
+    let headerOptions = document.querySelectorAll("ul li a");
     for (let i = 0; i < sections.length; i++) {
       let top = window.scrollY;
       let offset = sections[i].offsetTop - 150;
       let height = sections[i].offsetHeight;
-      let id = sections[i].getAttribute('id');
+      let id = sections[i].getAttribute("id");
       if (top >= offset && top < offset + height) {
         headerOptions.forEach((i) => {
-          i.classList.remove('active');
+          i.classList.remove("active");
         });
         document
           .querySelector('a[href="#' + id + '"]')
-          ?.classList.add('active');
+          ?.classList.add("active");
       }
     }
   }
   // section = 'intro';
-  title = 'portfolioSushma';
+  title = "portfolioSushma";
 }
