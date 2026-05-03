6bc6410288c338626a1039a97d6c91678e4334cb
Sushma <sushmashenoy1997@gmail.com>
2026-05-03 15:49:50 -0700

    Added work experience as contractor for Google, changed the resume

--- src/app/timeline/timeline.component.ts
+++ src/app/timeline/timeline.component.ts
@@ -8,34 +8,40 @@ import { Component } from "@angular/core";
 export class TimelineComponent {
   Projects = [
     {
+      role: "Associate Technology Consultant",
+      name: "Virtusa Pvt Ltd, Vendor: Google Inc.",
+      year: "Jun 2023 - Apr 2026",
+      tag: "Built scalable Angular applications for internal Google platforms, including vendor management and AI-driven sentiment analysis dashboards.Automated reporting workflows using Apps Script, improving operational efficiency by 30%",
+    },
+    {
       role: "Hybrid Application Developer",
-      name: "Axis Mutual Fund",
-      year: "Sep 2021- Oct 2022",
+      name: "Robosoft Technologies - Project: Axis Mutual Fund",
+      year: "Sep 2021 - Oct 2022",
       tag: "Worked on official hybrid(iOS, android, Web) application of Axis Mutual Fund(India) for investors. Worked on Angular version 8, ionic version 4. This project had won Best Project Award in the qaurterly meerting",
     },
     {
       role: "Hybrid Application Developer",
-      name: "L&T Mutual Fund",
-      year: "Apr 2020- Sep 2021",
+      name: "Robosoft Technologies - Project: L&T Mutual Fund",
+      year: "Apr 2020 - Sep 2021",
       tag: "Developed a distributor app for L&T mutual fund which manages the clients investments. This includes account statement view, Transaction History view, Folio deatils   ",
     },
 
     {
       role: "Angular Developer",
-      name: "Volonte - Resource Management Application",
-      year: "Jan 2020- Mar 2020",
+      name: "Robosoft Technologies - Project: Volonte - Resource Management Application",
+      year: "Jan 2020 - Mar 2020",
       tag: "Online resource management web application which helps in career change, upskilling or reskilling workforce. This is digital solution for gaining higher profitability by talent ",
     },
     {
       role: "ReactJS Developer",
-      name: "Noon - e-commerce platform",
-      year: "Oct 2019- Jan 2020",
+      name: "Robosoft Technologies - Project: Noon - e-commerce platform",
+      year: "Oct 2019 - Jan 2020",
       tag: "This is an online platform offering services and digital marketplace built using ReactJS. I Worked on internationalization of the web application, Loyalty program which focused on offers, deals for the purchases",
     },
     {
       role: "VanillaJS Developer",
-      name: "NoonPay",
-      year: "Jul 2019- Sep 2019",
+      name: "Robosoft Technologies - Project: NoonPay",
+      year: "Jul 2019 - Sep 2019",
       tag: "This is an online payment platform built using VanillaJS where I worked on building UI components for mobile and desktop versions",
     },
   ];
